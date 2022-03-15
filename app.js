98dc3709d70cd4dcb9d8ed8d5ea5c8e54734080f
AngelaC123 <angelarare@gmail.com>
2022-03-15 23:46:21 +0800

    Add  express-handlebars

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const port = 3000
 
 const app = express()
 
 require('./config/mongoose')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send(`This is a login system`)
+  res.render('index')
 })
 
 app.listen(port, () => {
