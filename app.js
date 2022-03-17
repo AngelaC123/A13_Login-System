42505f2267139e0478c2bf8b45eac7158cf1ea02
AngelaC123 <angelarare@gmail.com>
2022-03-17 12:11:40 +0800

    Add welcome page Can show user's first name if input data correct

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const User = require('./models/users.js')
 
 const port = 3000
 
 const app = express()
+const bodyParser = require('body-parser')
 
 require('./config/mongoose')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post('/', (req, res) => {
+  const { email, password } = req.body
+  return User.findOne({ email, password })
+    .lean()
+    .then((user) => {
+      res.render('welcome', { firstName: user.firstName })
+    })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Express server is now listening on http://localhost:${port}`)
